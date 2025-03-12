7d611923479f2f0bcd6ff1fd5a184a848e89463b
Gaby-de-Miranda <mg.portopes@gmail.com>
2025-03-12 08:29:20 -0300

    Update SerialCommunication.py
    Modificação no loop para melhor output no destinatário

--- SerialCommunication.py
+++ SerialCommunication.py
@@ -11,33 +11,45 @@
 # MICROPYTHON CODE
 
 from machine import UART, Pin
-from time import sleep, ticks_ms
+from machine import UART, Pin
+from time import sleep_ms as sleep
+from time import ticks_ms
 
-i = 0 # Variável que será repassada
-rep = 0 # Repique do pushbutton
-
+i = 0 # Variável a ser transmitida
+j = 0 # Variável de controle
+rep = 0 # Variável de repique do botão
 indicador = Pin(5, Pin.OUT) # LED conectado ao pino 5
 botao = Pin(4, Pin.IN, Pin.PULL_UP) # Pushbutton conectado ao pino 4
 
-# Função que registra o aperto do botão e muda o valor de i
+# Função de controle para o botão
 def apertoubotao(pino):
-    global rep
-    global i 
-    if ((ticks_ms() - rep) > 300):
+    global rep 
+    global i
+    global j
+    if ((ticks_ms()-rep) > 300):
         rep = ticks_ms()    
-        i += 1
-      
+        i = i + 1
+        j = j + 1
+        
+# Garante que o aperto do botão seja registrado ainda que o aperto ocorra durante o delay
 botao.irq(trigger=Pin.IRQ_FALLING, handler=apertoubotao)
 
 try:
-    # Iniciando a comunicação serial
+    # Iniciando comunicação serial
     uart1 = UART(1, baudrate=115200, tx=17, rx=16)
+
 except OSError as e:
-    print(f"Erro ao inicializar UART: {e}")  
+    # Indica o erro caso a conexão seja mal sucedida
+    print(f"Erro ao inicializar UART: {e}")
+    
 else:
-    # Liga a LED caso a conexão seja bem sucedida
+    # Acende o LED caso a conexão seja bem sucedida
     indicador.value(1)
     
 while True:
-    print(i)
-    sleep(2)
+    # Envia apenas valores novos, evitando repetições no output do destinatário
+    if (j == 1):
+        print(i)
+        j = j - 1
+    sleep(500) # Delay de 500 milisegundos
+    
